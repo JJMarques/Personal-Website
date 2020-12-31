47baa8881a361b7be29a24c9f743c6bcb57f7996
Jose Marques <jjgm98@gmail.com>
2020-12-31 14:01:22 +0000

    home page social media icons

--- components/Header.js
+++ components/Header.js
@@ -2,10 +2,19 @@ import React from 'react'
 import Link from 'next/link'
 import styles from '../styles/Header.module.css'
 import { BsArrowUpRight, BsPersonFill } from 'react-icons/bs'
+import { FiInstagram, FiGithub } from 'react-icons/fi'
 
 function Header() {
     return (
         <div className={styles.header}>
+            <div className={styles.social}>
+                <a href="https://www.instagram.com/jmarques_98/" target="_blank">
+                    <FiInstagram />
+                </a>
+                <a href="https://github.com/JJMarques" target="_blank" style={{ marginLeft: '30px' }}>
+                    <FiGithub />
+                </a>
+            </div>
             <h1 className={styles.name}>José Marques</h1>
             <div className={styles.roles}>
                 <span>Front-end Web Developer</span>
